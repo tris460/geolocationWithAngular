12d2e1e17b9f967f1590fde57cd331bdeb9a4037
Bety Martinez <trism460@gmail.com>
2021-09-25 09:49:40 -0500

    Add basic functions of a calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,34 @@ export class AppComponent {
   title = 'MyFirstApp';
   helloWorld: string;
   userName: string;
+  firstNumber: number;
+  secondNumber: number;
+  result: number;
   constructor() {
     this.helloWorld = "Hello World";
     this.userName = '';
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.result = 0;
   }
   greet(){
     alert("Hello " + this.userName);
   }
   clear(){
     this.userName ='';
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+  }
+  add() {
+    this.result = parseFloat(this.firstNumber.toString()) + parseFloat(this.secondNumber.toString());
+  }
+  substract() {
+    this.result = this.firstNumber - this.secondNumber;
+ }
+  multiply() {
+    this.result = this.firstNumber * this.secondNumber;
+  }
+  divide(){
+    this.result = this.firstNumber / this.secondNumber;
   }
 }
